505857b7f5f427c1c3259269bc8657b399842efb
anonymous9482 <aidric.miller@gmail.com>
2026-02-28 22:16:17 -0700

    Fixed coyote time
    pray this doesn't conflict

--- Scripts/protagonist.gd
+++ Scripts/protagonist.gd
@@ -4,8 +4,9 @@ class_name Protagonist extends CharacterBody2D
 @export var SPEED = 75.0
 const JUMP_VELOCITY = -175.0
 
-var falling = true;
+
 var floating = false;
+var floated = false
 
 var hasTorch = true;
 
@@ -20,7 +21,6 @@ func _physics_process(delta: float) -> void:
 	onFloor = is_on_floor()
 	if (inRope):
 		onFloor = true
-		falling = false;
 		print("should be on rope")
 		if (Input.is_action_pressed("Use Rope")):
 			velocity.y = 10
@@ -28,7 +28,7 @@ func _physics_process(delta: float) -> void:
 	handle_gravity(delta);
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and (onFloor or floating):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -43,25 +43,21 @@ func _physics_process(delta: float) -> void:
 
 func handle_gravity(delta: float): 
 	if !onFloor:
-		if velocity.y <= 0:
-			velocity += get_gravity() * delta;
-		if !floating:
-			floating = true;
+		velocity += get_gravity() * delta;
+		if !floating and !floated:
+			floating = true
 			$CoyoteTimer.start();
-			if !inRope:
+			if !inRope: #not sure what this does, so I kept it despite changing everything 
 				velocity += get_gravity() * delta * 0.01;
 				print("Slowly going down")
 	else:
-		falling = false;
 		floating = false;
-	if falling:
-		if (!inRope):
-			print("Falling")
-			velocity += get_gravity() * delta;
+		floated = false
 
 
 func _on_coyote_timer_timeout() -> void:
-	falling = true;
+	floating = false;
+	floated = true
 
 func gainTorch(energy: float):
 	$Torch/PointLight2D.energy = energy
